e5ff5adfa7d0df4984fa19ea0050e1ec334a0f13
ancovic <antcov0101@gmail.com>
2026-03-25 17:55:37 +0100

    Added ConsoleApp

---
+++ ancovicConApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ancovicConApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
